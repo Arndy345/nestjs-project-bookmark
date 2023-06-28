dd4cacb1e8595e7484395d25a62e044e5dd0af86
Nonso Andrew <nonsoandrew1@gmail.com>
2023-06-28 10:56:59 +0100

    Use the body decorator to access request body Used the argon2 package to hash user passwords Feat: signup and signin Feat: handled errors using nest provided errorHandlers

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
+import { error } from 'console';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signin() {
-    return `i am signed in`;
+  constructor(private prisma: PrismaService) {}
+  async signin(dto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+    if (!user)
+      throw new ForbiddenException(
+        'User not found',
+      );
+    try {
+      if (
+        await argon.verify(
+          user.hash,
+          dto.password,
+        )
+      ) {
+        delete user.hash;
+        return user;
+      } else {
+        return `Incorrect Password`;
+      }
+    } catch (err) {
+      throw new error('Server Error');
+    }
   }
 
-  signup() {
-    return `i am signed up`;
+  async signup(dto: AuthDto) {
+    try {
+      //generate hashed password
+      const hash = await argon.hash(dto.password);
+
+      //save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      //return saved user
+      return user;
+    } catch (e) {
+      if (
+        e instanceof
+        Prisma.PrismaClientKnownRequestError
+      ) {
+        // The .code property can be accessed in a type-safe manner
+        if (e.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credientials taken',
+          );
+        }
+      }
+      throw e;
+    }
   }
 }
