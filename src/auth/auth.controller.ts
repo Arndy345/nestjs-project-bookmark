f32416462a8b9e166bb03006946c96d890218a43
Nonso Andrew <nonsoandrew1@gmail.com>
2023-06-29 08:56:48 +0100

    chore: used nestjs httpCode module to set accurate status response codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,31 +2,27 @@ import {
   Body,
   Controller,
   Post,
-  Get,
-  UseGuards,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { AuthGuard } from './auth.guard';
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  // POST /auth
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
-  }
-
-  @UseGuards(AuthGuard)
-  @Get('auth')
-  auth() {
-    return 'Testing 1 2 3';
   }
 }
