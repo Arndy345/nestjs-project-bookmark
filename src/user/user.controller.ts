68b655ab3f8365a310a1a2db03becc5fe0464b1f
Nonso Andrew <nonsoandrew1@gmail.com>
2023-07-02 13:39:44 +0100

    Feat: add edit user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,18 +2,29 @@ import {
   Controller,
   Get,
   UseGuards,
-  Request,
+  Patch,
+  Body,
 } from '@nestjs/common';
 import { GetUser } from '../auth/decorators';
+import { EditUserDto } from './dto/edit-user.dto';
 import { UserService } from './user.service';
 import { AuthGuard } from '../auth';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
-  @UseGuards(AuthGuard)
+
   @Get('me')
   getMe(@GetUser('username') email: string) {
     return this.userService.getMe(email);
+  }
+
+  @Patch('me')
+  editUser(
+    @GetUser('username') email: string,
+    @Body() dto: EditUserDto,
+  ) {
+    return this.userService.editUser(email, dto);
   }
 }
