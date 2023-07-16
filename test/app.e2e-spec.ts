607d5a7cdb76b4ea27788f6944f65043bc8b5bf2
Nonso Andrew <nonsoandrew1@gmail.com>
2023-07-16 13:18:49 +0100

    bookmark tests updated

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,6 +7,10 @@ import { AppModule } from '../src';
 import { Test } from '@nestjs/testing';
 import { PrismaService } from '../src/prisma';
 import { EditUserDto } from 'src/user/dto/edit-user.dto';
+import {
+  BookmarkDto,
+  EditBookmarkDto,
+} from 'src/bookmark/dto';
 
 describe('App e2e', () => {
   // let token: string;
@@ -110,6 +114,72 @@ describe('App e2e', () => {
       });
     });
   });
+  describe('Bookmark', () => {
+    let token = 'hey';
+    beforeAll(async () => {
+      return request(app.getHttpServer())
+        .post('/auth/signin')
+        .send({
+          email: 'nonso1@gmail.com',
+          password: '1234',
+        })
+        .then((body) => {
+          token = body.body.access_token;
+        });
+    });
+    it('/POST creates new bookmark', () => {
+      const dto: BookmarkDto = {
+        title: 'Arena in here',
+        description: 'Here we go',
+        link: 'github.com',
+      };
+      return request(app.getHttpServer())
+        .get('/users/me')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+    });
+    it('/POST should return error when user is not authenticated', () => {
+      const dto: BookmarkDto = {
+        title: 'Arena in here',
+        description: 'Here we go',
+        link: 'github.com',
+      };
+      return request(app.getHttpServer())
+        .get('/users/me')
+        .set('Authorization', `Bearer token`)
+        .expect(401);
+    });
+    it('/GET bookmark', () => {
+      return request(app.getHttpServer())
+        .get('/bookmarks/')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+    });
+    it('/GET bookmark by id', () => {
+      return request(app.getHttpServer())
+        .get('/bookmarks/1')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+    });
+    it('/PATCH updates bookmark details', () => {
+      const dto: EditBookmarkDto = {
+        title: 'testing123@gmai.com',
+        description: 'Here we go',
+        link: 'github.com',
+      };
+      return request(app.getHttpServer())
+        .patch('/bookmarks/1')
+        .set('Authorization', `Bearer ${token}`)
+        .send(dto)
+        .expect(200);
+    });
+    it('/DELETE bookmark', () => {
+      return request(app.getHttpServer())
+        .delete('/bookmarks/1')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+    });
+  });
   describe('User', () => {
     let token = 'hey';
     beforeAll(async () => {
@@ -123,8 +193,6 @@ describe('App e2e', () => {
           token = body.body.access_token;
         });
     });
-
-    beforeEach(() => {});
 
     it('/GET user', () => {
       return request(app.getHttpServer())
@@ -144,17 +212,6 @@ describe('App e2e', () => {
         .send(dto)
         .expect(200);
     });
-  });
-
-  describe('Bookmark', () => {
-    describe('Create bookmarks', () => {});
-    describe('Get bookmarks', () => {});
-
-    describe('Get bookmarks by Id', () => {});
-
-    describe('Edit bookmarks', () => {});
-
-    describe('Delete bookmarks', () => {});
   });
 
   afterAll(() => {
